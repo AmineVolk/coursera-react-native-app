7124aaf7d10f2e487433fd22f3e6a2d504d76f60
amine <hamdi.amine.dev@gmail.com>
2020-03-16 19:29:48 +0100

    network info

--- components/Main/index.js
+++ components/Main/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import { Platform, StyleSheet, View } from "react-native";
+import { Platform, StyleSheet, View, ToastAndroid } from "react-native";
 import MainNavigator from "../MainNavigator";
 import PropTypes from 'prop-types'
-
+import NetInfo from '@react-native-community/netinfo';
 class MainComponent extends Component {
 
   componentDidMount() {
@@ -10,7 +10,39 @@ class MainComponent extends Component {
     this.props.fetchCommentsDispatch();
     this.props.fetchPromosDispatch();
     this.props.fetchLeadersDispatch();
+
+    NetInfo.fetch()
+      .then((connectionInfo) => {
+        ToastAndroid.show('Initial Network Connectivity Type: '
+          + connectionInfo.type + ', effectiveType: ' + connectionInfo.effectiveType,
+          ToastAndroid.LONG)
+      });
+
+    unsubscribe = NetInfo.addEventListener(this.handleConnectivityChange);
+
+
   }
+  componentWillUnmount() {
+    unsubscribe()
+  }
+
+  handleConnectivityChange = (connectionInfo) => {
+    switch (connectionInfo.type) {
+      case 'none':
+        ToastAndroid.show('Your are now offline !', ToastAndroid.LONG);
+        break;
+      case 'wifi':
+        ToastAndroid.show('Your are now connected to wifi !', ToastAndroid.LONG);
+        break;
+      case 'celular':
+        ToastAndroid.show('Your are now connected to celular !', ToastAndroid.LONG);
+        break;
+      case 'uknown':
+        ToastAndroid.show('Your have uknown connection !', ToastAndroid.LONG);
+        break;
+    }
+  }
+
   render() {
     return (
       <View style={{ flex: 1 }}>
